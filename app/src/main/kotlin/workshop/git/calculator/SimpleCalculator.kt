1cd6fa1e2892334a1438bc0bf38e39de8c0326df
Rafal Adamek <rafal.adamek@hotmail.com>
2023-11-11 18:38:21 +0100

    [API Integration] Add SimpleCalculator integration
    It is missing one instruction which need to be rebased

---
+++ app/src/main/kotlin/workshop/git/calculator/SimpleCalculator.kt
@@ -0,0 +1,10 @@
+package workshop.git.calculator
+
+class SimpleCalculator : CalculatorApi {
+
+    override suspend fun add(x: Int, y: Int): CalculationResult = CalculationResult.Success(x + y)
+
+    override suspend fun substract(x: Int, y: Int): CalculationResult = CalculationResult.Success(x - y)
+
+    override suspend fun divide(x: Int, y: Int): CalculationResult = CalculationResult.Success(x / y)
+}
