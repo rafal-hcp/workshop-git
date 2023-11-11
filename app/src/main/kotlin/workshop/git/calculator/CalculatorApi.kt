cb77a24dad10423323ab64afdd78b0373dc8b10f
Rafal Adamek <rafal.adamek@hotmail.com>
2023-11-11 18:35:42 +0100

    [api] Add test implementation for CalculatorApi
    This should not be here, remove this commit

--- app/src/main/kotlin/workshop/git/calculator/CalculatorApi.kt
+++ app/src/main/kotlin/workshop/git/calculator/CalculatorApi.kt
@@ -9,3 +9,13 @@ interface CalculatorApi {
 
     suspend fun divide(x: Int, y: Int): CalculationResult
 }
+
+class CalculatorApiTest: CalculatorApi {
+    override suspend fun add(x: Int, y: Int): CalculationResult = CalculationResult.Success(1)
+
+    override suspend fun substract(x: Int, y: Int): CalculationResult = CalculationResult.Success(2)
+
+    override suspend fun multiply(x: Int, y: Int): CalculationResult = CalculationResult.Success(3)
+
+    override suspend fun divide(x: Int, y: Int): CalculationResult = CalculationResult.Success(4)
+}
