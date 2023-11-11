eea7c8ef11666e49f48653e89bbd45626a621390
Rafal Adamek <rafal.adamek@hotmail.com>
2023-11-11 18:32:51 +0100

    [api] CalculatorApi
    Added an API for calculator
    It misses one instruction which will be added later as a fixup

---
+++ app/src/main/kotlin/workshop/git/calculator/CalculatorApi.kt
@@ -0,0 +1,9 @@
+package workshop.git.calculator
+
+interface CalculatorApi {
+    suspend fun add(x: Int, y: Int): CalculationResult
+
+    suspend fun substract(x: Int, y: Int): CalculationResult
+
+    suspend fun divide(x: Int, y: Int): CalculationResult
+}
