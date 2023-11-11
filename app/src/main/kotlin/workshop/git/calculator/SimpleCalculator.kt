8bbe7b1e5260cb93493aeef46c66376295582136
Rafal Adamek <rafal.adamek@hotmail.com>
2023-11-11 18:39:38 +0100

    [API integration] Add missing implementation
    Fixup to previous commit using rebase

--- app/src/main/kotlin/workshop/git/calculator/SimpleCalculator.kt
+++ app/src/main/kotlin/workshop/git/calculator/SimpleCalculator.kt
@@ -6,5 +6,7 @@ class SimpleCalculator : CalculatorApi {
 
     override suspend fun substract(x: Int, y: Int): CalculationResult = CalculationResult.Success(x - y)
 
+    override suspend fun multiply(x: Int, y: Int): CalculationResult = CalculationResult.Success(x * y)
+
     override suspend fun divide(x: Int, y: Int): CalculationResult = CalculationResult.Success(x / y)
 }
